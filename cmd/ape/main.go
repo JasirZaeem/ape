1628c6468b280f9cca5b8a97a3f017d4afd9384c
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-24 23:35:53 +0530

    Added rudimentary repl

--- cmd/ape/main.go
+++ cmd/ape/main.go
@@ -1,1 +1,22 @@
-package ape
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/JasirZaeem/ape/pkg/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+
+	fmt.Printf("Type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
