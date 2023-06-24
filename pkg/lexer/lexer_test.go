4788e604a421aded4309a0919b24aa94882ca461
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-24 22:54:35 +0530

    Add more single char operators to the lexel

--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -13,9 +13,11 @@ let ten = 10;
 
 let add = fn(x, y) {
 	x + y;
-}
+};
 
 let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
 `
 
 	tests := []struct {
@@ -47,6 +49,7 @@ let result = add(five, ten);
 		{token.IDENT, "y"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
 		{token.IDENT, "result"},
 		{token.ASSIGN, "="},
@@ -56,6 +59,18 @@ let result = add(five, ten);
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
