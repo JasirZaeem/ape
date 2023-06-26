3250af4b33d51a747c644837c805429bc97772fa
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-26 21:46:57 +0530

    Add expression statement struct to ast

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -45,6 +45,14 @@ type ReturnStatement struct {
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
 type Identifier struct {
 	Token token.Token
 	Value string
