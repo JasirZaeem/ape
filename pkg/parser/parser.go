e9777e3ae9903f7d30d11f129bcd67bdeba1850e
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-27 02:01:11 +0530

    Add operand handling functions to parser

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -7,13 +7,20 @@ import (
 	"github.com/JasirZaeem/ape/pkg/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(e ast.Expression) ast.Expression
+)
+
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 
-	errors []string
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -29,6 +36,14 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
