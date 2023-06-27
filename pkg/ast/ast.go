f2ae7ac564ca4cfafd17d92cb92aa4bb3ca7ff5c
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-27 22:59:57 +0530

    Add function literal to ast

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"github.com/JasirZaeem/ape/pkg/token"
+	"strings"
 )
 
 type Node interface {
@@ -208,6 +209,31 @@ func (ie *IfExpression) String() string {
 		out.WriteString(" else ")
 		out.WriteString(ie.Alternative.String())
 	}
+
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteByte('(')
+	out.WriteString(strings.Join(params, ","))
+	out.WriteByte(')')
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
