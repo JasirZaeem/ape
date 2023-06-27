8b5ddf2335adad34e1cdd7fca5c00ce9872e4607
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-27 15:07:03 +0530

    Add test for integer literal parsing

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -151,3 +151,35 @@ func TestIdentifierExpression(t *testing.T) {
 		t.Errorf("ident.TokenLiteral not %s. got = %s", "foobar", ident.TokenLiteral())
 	}
 }
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "42;"
+
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not have len 1. got = %d",
+			len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] not ast.ExpressionStatement. got = %T",
+			program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got = %T",
+			stmt.Expression)
+	}
+	if literal.Value != "5" {
+		t.Errorf("ident.Value not %s. got = %s", "5", literal.Value)
+	}
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("ident.TokenLiteral not %s. got = %s", "5", literal.TokenLiteral())
+	}
+}
