cc978e4b7f87ad916f8c2264f4f6c45c19efe780
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-27 18:38:02 +0530

    Add infix expression to ast

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -137,3 +137,24 @@ func (pe *PrefixExpression) String() string {
 
 	return out.String()
 }
+
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteByte('(')
+	out.WriteString(ie.Left.String())
+	out.WriteString(ie.Operator)
+	out.WriteString(ie.Right.String())
+	out.WriteByte(')')
+
+	return out.String()
+}
