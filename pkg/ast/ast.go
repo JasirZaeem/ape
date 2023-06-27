e4ad35dbbeeeb14bc66507fcb0422a44cf0db2df
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-27 20:47:19 +0530

    Add block statement to ast

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -101,6 +101,23 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
