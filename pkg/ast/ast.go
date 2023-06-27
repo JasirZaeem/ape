e5c5a8e210780b01690ca2f05a0bdfcbe60c1089
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-27 15:50:12 +0530

    Add prefix expression to ast

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -118,3 +118,22 @@ type IntegerLiteral struct {
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteByte('(')
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteByte(')')
+
+	return out.String()
+}
