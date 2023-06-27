eb223914ce4de0df3c1272c999ba728b4e00c4e8
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-28 00:56:20 +0530

    Add call expression in ast

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -237,3 +237,27 @@ func (fl *FunctionLiteral) String() string {
 
 	return out.String()
 }
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode()      {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteByte('(')
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteByte(')')
+
+	return out.String()
+}
