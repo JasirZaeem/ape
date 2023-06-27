71f45d56ced7972c5bbc23a49b9c9bcc0f0ea232
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-28 01:36:44 +0530

    Update let statement parsing to parse value expression

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -134,7 +134,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
