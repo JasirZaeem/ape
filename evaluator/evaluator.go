afc558f6868092b544cc6790f3f53fa91a223845
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-28 20:27:05 +0530

    Add condition error checking for if expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -176,6 +176,9 @@ func isTruthy(obj object.Object) bool {
 
 func evalIfExpression(ie *ast.IfExpression) object.Object {
 	condition := Eval(ie.Condition)
+	if isError(condition) {
+		return condition
+	}
 
 	if isTruthy(condition) {
 		return Eval(ie.Consequence)
