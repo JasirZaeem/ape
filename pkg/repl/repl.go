53938e6b51772a3f9f60f4ff8a927a7a2da348a0
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-28 20:49:19 +0530

    Move environment out of loop to persist in repl.go

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -15,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -33,7 +34,6 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		env := object.NewEnvironment()
 		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
