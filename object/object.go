153e092cf05aa42b2f9fa5ea8016cd4ecfc87fca
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-28 12:53:09 +0530

    Fix the return type for Type func on objects

--- object/object.go
+++ object/object.go
@@ -19,17 +19,17 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Type() string    { return INTEGER_OBJ }
-func (i *Integer) Inspect() string { return strconv.FormatInt(i.Value, 10) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return strconv.FormatInt(i.Value, 10) }
 
 type Boolean struct {
 	Value bool
 }
 
-func (b *Boolean) Type() string    { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string { return strconv.FormatBool(b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return strconv.FormatBool(b.Value) }
 
 type Null struct{}
 
-func (n *Null) Type() string   { return NULL_OBJ }
-func (n *Null) string() string { return "null" }
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) string() string   { return "null" }
