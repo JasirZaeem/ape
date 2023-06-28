153a3931b25251b400ba42f7f3ca1d2817db5f86
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-29 00:36:00 +0530

    Add nesting to environments

--- object/environment.go
+++ object/environment.go
@@ -1,15 +1,25 @@
 package object
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
-	return &Environment{store: map[string]Object{}}
+	return &Environment{store: map[string]Object{}, outer: nil}
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
