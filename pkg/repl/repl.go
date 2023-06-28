910ebf64bdb983e8c6db7d35775062aeb4f80bd6
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-28 13:13:52 +0530

    Update repl to show evaluated result

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/JasirZaeem/ape/evaluator"
 	"github.com/JasirZaeem/ape/pkg/parser"
 	"io"
 
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
