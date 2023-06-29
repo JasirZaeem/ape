8057ac24bf9974a4ce1bfe21922b720a8bada938
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-06-29 12:09:34 +0530

    Add test for string lexing

--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -27,6 +27,13 @@ if (5 < 10) {
 
 10 == 10;
 10 != 9;
+"foobar"
+"foo bar"
+"foo
+bar"
+"foo\"bar"
+"foo\\bar"
+"foo\nbar"
 `
 
 	tests := []struct {
@@ -106,6 +113,12 @@ if (5 < 10) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
+		{token.STRING, "foo\nbar"},
+		{token.STRING, "foo\"bar"},
+		{token.STRING, "foo\\bar"},
+		{token.STRING, "foo\nbar"},
 		{token.EOF, ""},
 	}
 
