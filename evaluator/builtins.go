f7aa5ca16716a808a7448037a5f3a5c9c704a66f
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-04 18:32:42 +0530

    Add type builtin function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -33,4 +33,13 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
+	// Type utilities
+	"type": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got = %d, want = 1", len(args))
+			}
+			return &object.String{Value: string(args[0].Type())}
+		},
+	},
 }
