4e1d16496d29535cf5e4abad30bb706448940164
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-09 22:15:46 +0530

    Add example for array functions

--- playground/src/codeExamples.ts
+++ playground/src/codeExamples.ts
@@ -32,8 +32,35 @@ export const factorial = `let fact = fn (n) {
 fact(5)
 `;
 
+export const arrayExamples = `
+let arr = [1, 2, 3, 4, 5];
+print("arr == ", arr);
+
+print("arr[0] == ", arr[0]);
+print("arr[1] == ", arr[1]);
+
+print("first(arr) == ", first(arr));
+print("last(arr) == ", last(arr));
+
+print("rest(arr) == ", rest(arr));
+print("rest(rest(arr)) == ", rest(rest(arr)));
+print("init(arr) == ", init(arr));
+print("init(init(arr)) == ", init(init(arr)));
+
+print("push(arr, 6) == ", push(arr, 6));
+print("pop(arr) == ", pop(arr));
+print("push_front(arr, 0) == ", push_front(arr, 0));
+print("pop_front(arr) == ", pop_front(arr));
+
+print("insert(arr, 2, 10) == ", insert(arr, 2, 10));
+print("remove(arr, 2) == ", remove(arr, 2));
+
+print("reverse(arr) == ", reverse(arr));
+`;
+
 export const examples = {
   Fibonacci: fibonacci,
   "Tower of Hanoi": towerOfHanoi,
   Factorial: factorial,
+  Arrays: arrayExamples,
 };
