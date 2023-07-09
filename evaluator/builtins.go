aab13073a6b66293a761176aedd255008cd8e84b
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-09 20:19:00 +0530

    Add more array builtins to ape

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -166,4 +166,154 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got = %d, want = 1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got = %d, want = 2", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+			return &object.Array{Elements: newElements}
+		},
+	},
+	"pop": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got = %d, want = 1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `pop` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[0:length-1])
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
+		},
+	},
+	"push_front": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got = %d, want = 2", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `push_front` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements[1:], arr.Elements)
+			newElements[0] = args[1]
+			return &object.Array{Elements: newElements}
+		},
+	},
+	"pop_front": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got = %d, want = 1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `pop_front` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
+		},
+	},
+	"insert": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 3 {
+				return newError("wrong number of arguments. got = %d, want = 3", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `insert` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			index, ok := args[1].(*object.Integer)
+			if !ok {
+				return newError("argument to `insert` must be INTEGER, got %s", args[1].Type())
+			}
+			if index.Value < 0 || index.Value > int64(length) {
+				return newError("index out of range: %d", index.Value)
+			}
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, arr.Elements[0:index.Value])
+			newElements[index.Value] = args[2]
+			copy(newElements[index.Value+1:], arr.Elements[index.Value:length])
+			return &object.Array{Elements: newElements}
+		},
+	},
+	"remove": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got = %d, want = 2", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `remove` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			index, ok := args[1].(*object.Integer)
+			if !ok {
+				return newError("argument to `remove` must be INTEGER, got %s", args[1].Type())
+			}
+			if index.Value < 0 || index.Value >= int64(length) {
+				return newError("index out of range: %d", index.Value)
+			}
+			newElements := make([]object.Object, length-1, length-1)
+			copy(newElements, arr.Elements[0:index.Value])
+			copy(newElements[index.Value:], arr.Elements[index.Value+1:length])
+			return &object.Array{Elements: newElements}
+		},
+	},
+	"reverse": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got = %d, want = 1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `reverse` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length, length)
+			for i := 0; i < length; i++ {
+				newElements[i] = arr.Elements[length-i-1]
+			}
+			return &object.Array{Elements: newElements}
+		},
+	},
 }
