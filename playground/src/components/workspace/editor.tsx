634b339c1c7446def6c72740e4a0083b4c525e5e
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-15 19:24:07 +0530

    Add correct theme to editor based on user choice

--- playground/src/components/workspace/editor.tsx
+++ playground/src/components/workspace/editor.tsx
@@ -1,11 +1,17 @@
-import { nightOwlInit } from "@/editor/themes/night-owl.ts";
+import { nightOwl } from "@/editor/themes/night-owl.ts";
 import { StreamLanguage } from "@codemirror/language";
 import { apeMode } from "@/editor/apeMode.ts";
 import CodeMirror from "@uiw/react-codemirror";
 import { useApe } from "@/apeContext.tsx";
+import { lightOwl } from "@/editor/themes/light-owl.ts";
+import { useTheme } from "@/themeContext.tsx";
 
 export function Editor() {
   const { code, setCode, setSelectedCode } = useApe();
+
+  const { effectiveTheme } = useTheme();
+
+  const theme = effectiveTheme === "light" ? lightOwl : nightOwl;
 
   return (
     <CodeMirror
@@ -14,7 +20,7 @@ export function Editor() {
       onChange={(val) => {
         setCode(val);
       }}
-      theme={nightOwlInit()}
+      theme={theme}
       extensions={[StreamLanguage.define(apeMode)]}
       onStatistics={(stats) => {
         setSelectedCode(stats.selectionCode.trim());
