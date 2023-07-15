71e5a5c4c0bf05c4e2b1dcfd3f1807b1141ec2a4
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-15 18:12:53 +0530

    Create night owl theme for ast viewer.

--- playground/src/components/workspace/astViewer.tsx
+++ playground/src/components/workspace/astViewer.tsx
@@ -15,12 +15,29 @@ export function AstViewer() {
       return prevAst;
     });
   }, [code]);
-
   return (
-    <div className="overflow-auto w-full h-full">
+    <div className="overflow-auto w-full h-full bg-[#011627] pl-2">
       <JSONTree
         data={ast}
         shouldExpandNodeInitially={(_, __, level) => level < 3}
+        theme={{
+          base00: "#011627",
+          base01: "#ff5874",
+          base02: "#addb67",
+          base03: "#f78c6c",
+          base04: "#82aaff",
+          base05: "#c792ea",
+          base06: "#7fdbca",
+          base07: "#eeeeee",
+          base08: "#637777",
+          base09: "#ff5874",
+          base0A: "#addb67",
+          base0B: "#f78c6c",
+          base0C: "#82aaff",
+          base0D: "#c792ea",
+          base0E: "#7fdbca",
+          base0F: "#eeeeee",
+        }}
       />
     </div>
   );
