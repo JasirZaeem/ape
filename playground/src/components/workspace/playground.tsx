0fc1b5f807812daca81b2e3943357d1874bed847
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-15 20:01:11 +0530

    Rework ast panel visibility control

--- playground/src/components/workspace/playground.tsx
+++ playground/src/components/workspace/playground.tsx
@@ -8,38 +8,45 @@ import { DotsHorizontalIcon, DotsVerticalIcon } from "@radix-ui/react-icons";
 import { Repl } from "@/components/repl";
 import { Editor } from "@/components/workspace/editor.tsx";
 import { AstViewer } from "@/components/workspace/astViewer.tsx";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { useApe } from "@/apeContext.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 export function Playground() {
   const astPanelRef = useRef<ImperativePanelHandle>(null);
 
-  const { astViewerVisible } = useApe();
-
-  useEffect(() => {
-    if (!astViewerVisible) {
-      astPanelRef.current?.collapse();
-    } else {
-      astPanelRef.current?.expand();
-    }
-  }, [astViewerVisible]);
+  const { astViewerVisible, setAstViewerVisible } = useApe();
 
   return (
     <PanelGroup direction="vertical">
       <Panel>
-        <PanelGroup direction="horizontal">
-          <Panel>
+        <PanelGroup direction="horizontal" className="relative">
+          <Panel className="rounded-r">
             <Editor />
           </Panel>
-          <PanelResizeHandle className="relative flex flex-col justify-center align-middle group bg-border w-2">
-            <DotsVerticalIcon className="absolute mx-auto -translate-x-1/4 invisible group-hover:visible" />
-          </PanelResizeHandle>
-          <Panel collapsible ref={astPanelRef}>
-            <AstViewer />
-          </Panel>
+          {astViewerVisible ? (
+            <>
+              <PanelResizeHandle className="relative flex flex-col justify-center align-middle group hover:bg-border w-2">
+                <DotsVerticalIcon className="absolute mx-auto -translate-x-1/4 invisible group-hover:visible" />
+              </PanelResizeHandle>
+              <Panel ref={astPanelRef} minSize={20} className="rounded-l">
+                <AstViewer />
+              </Panel>
+            </>
+          ) : (
+            <Button
+              className="absolute right-0 top-5 rounded-b-none -rotate-90 translate-x-1/4"
+              variant="outline"
+              onClick={() => {
+                setAstViewerVisible(true);
+              }}
+            >
+              AST
+            </Button>
+          )}
         </PanelGroup>
       </Panel>
-      <PanelResizeHandle className="relative flex justify-center align-middle group bg-border h-2">
+      <PanelResizeHandle className="relative flex justify-center align-middle group hover:bg-border h-2">
         <DotsHorizontalIcon className="absolute my-auto -translate-y-1/4 invisible group-hover:visible" />
       </PanelResizeHandle>
       <Panel>
