58761a7fd6e0406143324997abdd6f9cd6ba2310
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-07-17 16:20:57 +0530

    Check that correct number of params are passed to functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -489,6 +489,9 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 func applyFunction(fn object.Object, args []object.Object) object.Object {
 	switch fn := fn.(type) {
 	case *object.Function:
+		if len(fn.Parameters) != len(args) {
+			return newError("wrong number of arguments: want=%d, got=%d", len(fn.Parameters), len(args))
+		}
 		extendedEnv := extendFunctionEnv(fn, args)
 		evaluated := Eval(fn.Body, extendedEnv)
 		return unwrapReturnValue(evaluated)
